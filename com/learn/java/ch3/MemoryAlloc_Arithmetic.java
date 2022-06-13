40fab8d43d759d8460b6f0824e440b7a729fa6f7
Subaletchumi <107242167+Subaletchumi@users.noreply.github.com>
2022-06-13 22:32:36 +0530

    Create MemoryAlloc_Arithmetic.java

---
+++ com/learn/java/ch3/MemoryAlloc_Arithmetic.java
@@ -0,0 +1,18 @@
+
+public class MemoryAlloc_Arithmetic
+{
+	public static void main(String args[])
+	{
+		int a,b,c,d;
+		a = -5 + 8 * 6;
+		b = (55+9) % 9;
+		c = 20 + -3*5 / 8;
+		d = 5 + 15 / 3 * 2 - 8 % 3;
+
+		System.out.println("Output of a  is :"+a);
+		System.out.println("Output of b  is :"+b);
+		System.out.println("Output of c  is :"+c);
+		System.out.println("Output of d  is :"+d);
+	}
+
+}
