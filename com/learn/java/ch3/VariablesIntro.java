3e2d454b2e182306682bcc07a4299a0484cdd22c
Subaletchumi <107242167+Subaletchumi@users.noreply.github.com>
2022-06-13 20:32:27 +0530

    Create VariablesIntro.java

---
+++ com/learn/java/ch3/VariablesIntro.java
@@ -0,0 +1,20 @@
+
+public class VariablesIntro 
+{
+	public static void main(String args[])
+		{
+			int a;
+			a= 10;
+			System.out.println("Intialized variable a value is "+a);
+
+		
+			int b;
+			a = b = 20;
+			System.out.println("Now B value is "+b);
+			
+			int c;
+			c = a;
+			System.out.println("Now C value is "+c);
+		}
+
+}
