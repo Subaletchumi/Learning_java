2c4168728da1767e1d74fd0c8dcf20c248cf5fc9
Subaletchumi <107242167+Subaletchumi@users.noreply.github.com>
2022-06-13 20:36:02 +0530

    Create Person.java

---
+++ com/learn/java/ch3/Person.java
@@ -0,0 +1,82 @@
+import java.util.*;
+public class Person
+{
+
+
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Your Name");
+		String a = sc.nextLine();
+			
+		System.out.print("Gender");
+		String b = sc.nextLine();
+			
+		System.out.print("Enter your Date of Birth");
+		String c = sc.nextLine();
+			
+		System.out.print("Marital Status");
+		String d = sc.nextLine();
+		
+		System.out.print("Door Number");
+		String e = sc.nextLine();
+		
+		System.out.print("Street");
+		String f = sc.nextLine();
+		
+		System.out.print("Locality");
+		String g = sc.nextLine();
+		
+		System.out.print("City");
+		String h = sc.nextLine();
+		
+		System.out.print("State");
+		String i = sc.nextLine();
+		
+		System.out.print("Country");
+		String j = sc.nextLine();
+		
+		System.out.print("pincode");
+		String k = sc.nextLine();
+		
+		System.out.print("Aadhar Number");
+		String l = sc.nextLine();
+		
+		System.out.print("Telephone number is available?");
+		String m = sc.nextLine();
+		
+		System.out.print("Phone number");
+		String n = sc.nextLine();
+		
+		System.out.print("Is salaried?");
+		String o = sc.nextLine();
+		
+		System.out.print("Monthly salary");
+		String p = sc.nextLine();
+		
+		System.out.print("Monthly Expenes ");
+		String q = sc.nextLine();
+		
+		System.out.print("Hobbies");
+		String r = sc.nextLine();
+		System.out.println(" ***********************Your Personal Data is ***************************");
+		System.out.println("Your name is :"+a);
+		System.out.println("Gender is :"+b);
+		System.out.println("DOB is :"+c);
+		System.out.println("Marital status :"+d);
+		System.out.println("Door Number :"+e);
+		System.out.println("Street :"+f);
+		System.out.println("Locality :"+g);
+		System.out.println("City :"+h);
+		System.out.println("State :"+i);
+		System.out.println("Country :"+j);
+		System.out.println("Pincode :"+k);
+		System.out.println("Aadhar Number is :"+l);
+		System.out.println("Is Telephone number is Available :"+m);
+		System.out.println("Phone Number :"+n);
+		System.out.println("IsSalaried :"+o);
+		System.out.println("Monthly salary :"+p);
+		System.out.println("Monthly Expences :"+q);
+		System.out.println("Hobbies :"+r);
+	}
+}
