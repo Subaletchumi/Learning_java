3344d5e491d75f44269a8a0ad418866d03c5c625
Subaletchumi <107242167+Subaletchumi@users.noreply.github.com>
2022-06-13 22:22:41 +0530

    Create LimitCheck.java

---
+++ com/learn/java/ch3/LimitCheck.java
@@ -0,0 +1,17 @@
+public class LimitCheck {
+
+	public static void main(String[] args) {
+	
+		{
+		    for (char a = 0; a <= 225; a++)
+		        System.out.println(a);
+		        return;
+		}
+
+	}
+
+}
+
+Observed: Characters  printed infinitely.
+
+        	                          !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~????????????????????????????????  ¡ ¢ £ ¤ ¥ ¦ § ¨ © ª « ¬ ­ ® ¯ ° ± ² ³ ´ µ ¶ · ¸ ¹ º » ¼ ½ ¾ ¿ À Á Â Ã Ä Å Æ Ç È É Ê Ë Ì Í Î Ï Ð Ñ Ò Ó Ô Õ Ö × Ø Ù Ú Û Ü Ý Þ ß à á
