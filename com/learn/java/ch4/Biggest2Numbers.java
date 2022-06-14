da18641a2391ed4f8589e1d6456a9e00e97d9a67
Subaletchumi <107242167+Subaletchumi@users.noreply.github.com>
2022-06-14 21:02:56 +0530

    Create Biggest2Numbers.java

---
+++ com/learn/java/ch4/Biggest2Numbers.java
@@ -0,0 +1,18 @@
+public class Biggest2Numbers
+
+//USing ternary operator
+{
+  public static void main (String[]args)
+  {
+    int num1 = 50, num2 = 10, temp;
+
+    if (num1 == num2)
+      System.out.println ("Both are Equal\n");
+    else
+      {
+	temp = num1 > num2 ? num1 : num2;
+	System.out.println (temp + " is largest");
+      }
+
+  }
+}
